9edd03f1e997cc37f725d061b38023b633f881fe
zenxxn <bunbun2438@naver.com>
2023-08-25 16:56:08 +0900

    08/25 1차 실습 프로젝트 account 과제 진행중

--- src/main/java/com/example/account/controller/AccountController.java
+++ src/main/java/com/example/account/controller/AccountController.java
@@ -1,22 +1,36 @@
 package com.example.account.controller;
 
 import com.example.account.domain.Account;
+import com.example.account.dto.CreateAccount;
 import com.example.account.service.AccountService;
+import com.example.account.service.RedisTestService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequiredArgsConstructor
 public class AccountController {
     private final AccountService accountService;
+    private final RedisTestService redisTestService;
 
-    @GetMapping("/create-account")
-    public String createAccount(){
-        accountService.createAccount();
-        return "success";
+    @PostMapping ("/account")
+    public CreateAccount.Response createAccount(
+            @RequestBody @Valid CreateAccount.Request request
+            ){
+        accountService.createAccount(
+                request.getUserId(),
+                request.getInitialBalance()
+        );
+        return null;
     }
+
+    @GetMapping("/get-lock")
+    public String getLock() {
+        return redisTestService.getLock();
+    }
+
 
     @GetMapping("/account/{id}")
     public Account getAccount(@PathVariable Long id){
--- src/main/java/com/example/account/service/AccountService.java
+++ src/main/java/com/example/account/service/AccountService.java
@@ -1,8 +1,11 @@
 package com.example.account.service;
 
 import com.example.account.domain.Account;
-import com.example.account.domain.AccountStatus;
+import com.example.account.domain.AccountUser;
+import com.example.account.exception.AccountException;
 import com.example.account.repository.AccountRepository;
+import com.example.account.repository.AccountUserRepository;
+import com.example.account.type.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -12,18 +15,25 @@ import javax.transaction.Transactional;
 @RequiredArgsConstructor
 public class AccountService {
     private final AccountRepository accountRepository;
-
+    private final AccountUserRepository accountUserRepository;
+    /**
+     * 사용자가 있는지 조회하고,
+     * 계좌의 번호를 생성하고,
+     * 계좌를 저장하고, 그 정보를 넘긴다
+     */
     @Transactional
-    public void createAccount(){
-        Account account = Account.builder()
-                .accountNumber("40000")
-                .accountStatus(AccountStatus.IN_USE)
-                .build();
-        accountRepository.save(account);
+    public void createAccount(Long userId, Long initialBalance){
+        AccountUser accountUser = accountUserRepository.findById(userId)
+                .orElseThrow(() -> new AccountException(ErrorCode.USER_NOT_FOUND));
+
+        String newAccountNumber;
     }
 
     @Transactional
     public Account getAccount(Long id){
+        if (id < 0) {
+            throw new RuntimeException("Minus");
+        }
         return accountRepository.findById(id).get();
     }
 }
